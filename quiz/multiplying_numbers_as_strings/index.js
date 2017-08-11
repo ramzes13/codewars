13a97157f66cedc12493c920fd3e1bda9e9cf2e5
Darii Petru <pdarii@pentalog.fr>
2017-08-11 20:34:43 +0300

    finish

--- quiz/multiplying_numbers_as_strings/index.js
+++ quiz/multiplying_numbers_as_strings/index.js
@@ -1,20 +1,8 @@
 module.exports = class multiplying_numbers_as_strings {
-  multiply(a, b) {
-    let matrice = [];
-    let ROW = a.length;
-    let COL = b.length;
 
-    for (let i = 0; i < ROW; i++) {
-      matrice[i] = [];
-      for (let j = 0; j < COL; j++) {
-        let partResult = parseInt(a[i]) * parseInt(b[j]);
-        let upperResult = parseInt(partResult / 10);
-        let lowerResult = partResult - upperResult * 10;
-        matrice[i][j] = { up: upperResult, down: lowerResult, debug: partResult };
-        // matrice[i][j] = { i: i, j: j };
-      }
-    }
-    // console.log(matrice);
+  generateDiagonals(matrice, ROW, COL) {
+    let diagonals = [];
+    // console.log(ROW, COL, matrice)
 
     // There will be ROW+COL-1 lines in the output
     for (let line = 1; line <= (ROW + COL - 1); line++) {
@@ -26,17 +14,73 @@ module.exports = class multiplying_numbers_as_strings {
       /* Get count of elements in this line. The count of elements is
       equal to minimum of line number, COL-start_col and ROW */
       let count = Math.min(line, (COL - start_col), ROW);
+      let diagonalsRow = [];
 
       /* Print elements of this line */
       for (let j = 0; j < count; j++) {
-        console.log(matrice[Math.min(ROW, line) - j - 1][start_col + j])
-      }
-      // console.log(matrice[Math.min(ROW, line)-j-1][start_col+j])
-      // printf("%5d ", matrix[Math.min(ROW, line)-j-1][start_col+j]);
+        let value = matrice[Math.min(ROW, line) - j - 1][start_col + j];
 
-      /* Ptint elements of next diagonal on next line */
-      console.log("\n");
+        diagonalsRow.push(value);
+      }
+
+      diagonals.push(diagonalsRow);
+
     }
-    return "6";
+    return diagonals;
+  }
+  generateLatticeMatrice(a, b) {
+    let matrice = [];
+    let ROW = a.length;
+    let COL = b.length;
+    for (let i = 0; i < ROW; i++) {
+      matrice[i] = [];
+      for (let j = 0; j < COL; j++) {
+        let partResult = parseInt(a[i]) * parseInt(b[j]);
+        let upperResult = parseInt(partResult / 10);
+        let lowerResult = partResult - upperResult * 10;
+        matrice[i][j] = { up: upperResult, down: lowerResult, debug: partResult };
+      }
+    }
+
+    return matrice;
+  }
+
+  calculateSum(diagonal, previousDiagonal) {
+    let downSum = 0, upSum = 0;
+
+    if (diagonal) {
+      downSum = diagonal.reduce((accumulator, value) => {
+        return accumulator + value.down;
+      }, 0);
+    }
+
+    if (previousDiagonal) {
+      upSum = previousDiagonal.reduce((accumulator, value) => {
+        return accumulator + value.up;
+      }, 0);
+    }
+
+    return downSum + upSum;
+  }
+
+  multiply(a, b) {
+    let matrice = this.generateLatticeMatrice(a, b);
+    let diagonals = this.generateDiagonals(matrice, a.length, b.length);
+    let mindValue = 0;
+    let sumResponse = [];
+
+    for (let i = diagonals.length - 1; i >= -1; i--) {
+      let currentDiagonal = diagonals[i];
+      let previousDiagonal = diagonals[i + 1];
+      let rowSum = this.calculateSum(currentDiagonal, previousDiagonal) + mindValue;
+      if (rowSum >= 10) {
+        mindValue = parseInt(rowSum / 10);
+        rowSum = rowSum - mindValue * 10;
+      } else {
+        mindValue = 0;
+      }
+      sumResponse.push(rowSum);
+    }
+    return sumResponse.reverse().join('').replace(/^0+/, '');
   }
 }
--- test/quiz/multiplying_numbers_as_strings/index.test.js
+++ test/quiz/multiplying_numbers_as_strings/index.test.js
@@ -6,6 +6,7 @@ const multiplyObj = new multiplying_numbers_as_strings();
 
 describe('Some simple multiplications', function () {
   it('simple', function () {
-    Test.equal(multiplyObj.multiply("5479", "787"), "6");
+    Test.equal(multiplyObj.multiply("2", "3"), "6");
+    Test.equal(multiplyObj.multiply("30", "69"), "2070");
   });
 });
